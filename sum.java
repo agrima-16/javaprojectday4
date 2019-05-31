90cf5bdf58c134f08862436c0e5eda760e41d74d
agrima-16 <51071442+agrima-16@users.noreply.github.com>
2019-05-31 12:05:01 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,14 @@
+package com.projectday2;
+
+public class sum {
+
+	public static void main(String[] args) {
+	     sum m=new sum();
+		 System.out.println(m.sum(2, 3));	
+
+	}
+	public int sum(int a,int b){
+		return a+b;
+	}
+
+}
